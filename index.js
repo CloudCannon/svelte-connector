444025ac42f6e67eb98cfa40cbe414c55960dff8
Nathan Kennedy <nathan.kennedy.1997@gmail.com>
2021-10-21 09:13:24 +1300

    Initialized npm package

---
+++ index.js
@@ -0,0 +1,4 @@
+import { onCloudCannonChanges, stopCloudCannonChanges } from '@cloudcannon/visual-editor-connector';
+
+// These will be fleshed out in future, hence separate package
+export { onCloudCannonChanges, stopCloudCannonChanges };
